0142038cfa7006a90b8bc24d8bb15b14df1bdbc2
AnaLuisaFariaGomes <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-05-29 08:49:13 -0300

    Atividade 19.java

---
+++ 19.java
@@ -0,0 +1,16 @@
+  public static boolean estaCresc(No inicio) {
+    
+      if (inicio == null || inicio.proximo == null) {
+          return true;
+      }
+      No atual = inicio;
+
+        while (atual.proximo != null) {
+          
+            if (atual.dado > atual.proximo.dado) {
+                return false;
+            }
+            atual = atual.proximo;
+        }
+    return true;
+}
